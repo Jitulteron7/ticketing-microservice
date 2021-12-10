74db1ec22d16c695434eccfa022e4ada0b11895e
Jitul Teron <jitulteron9@gmail.com>
2021-12-10 20:03:31 +0530

    all starting message

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -9,7 +9,7 @@ import {PaymentCreatedListener} from "./events/listeners/paymentCreatedListener"
 
 
 const start = async () => {
-  console.log('starting ...')
+  console.log('starting .......')
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
--- payments/src/index.ts
+++ payments/src/index.ts
@@ -6,6 +6,8 @@ import {OrderCreatedListener} from "./events/listeners/orderCreatedListener"
 
 
 const start = async () => {
+
+  console.log("starting...")
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
